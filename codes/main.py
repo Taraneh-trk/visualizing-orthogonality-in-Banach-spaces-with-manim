3c6859349adf90a6accf343b3c01d4ed148391b6
Taraneh <145665647+Taraneh-trk@users.noreply.github.com>
2025-12-31 15:34:06 +0330

    step 2 : add scene 2 (defination of Banach Spaces)

--- codes/main.py
+++ codes/main.py
@@ -4,7 +4,7 @@ def StyleSetter_BasedOnTextType(text_type:str):
         if text_type == "title":
             return {"font_size": 48, "color": WHITE, "weight": BOLD, "should_center": True, "font": "Arial"}
         elif text_type == "subtitle":
-            return {"font_size": 36, "color": WHITE, "weight": ITALIC}
+            return {"font_size": 40, "color": WHITE}
         else:
             return {"font_size": 24, "color": WHITE}
 
@@ -19,7 +19,8 @@ class DisplayScene(Scene):
         # add scence 1
         # ------------------------------ 
         # 1-1 : title
-        text_title = TextScene(1, "Kinds of Orthogonality in Banach Spaces", 2, "title")
+
+        text_title = TextScene(1, "Kinds of Orthogonality in Banach Spaces", 1, "title")
         self.play(FadeIn(text_title.text))
         self.wait(text_title.wait_time)
 
@@ -29,11 +30,47 @@ class DisplayScene(Scene):
         # 3 . fade out the other
 
         text_banach = Text("Banach Spaces", **StyleSetter_BasedOnTextType("title"))
-        text_banach.move_to(text_title.text.get_center())
+        text_banach.move_to(text_title.text.get_center()).set_font_size(58)
         self.play(Transform(text_title.text, text_banach))
         self.play(FadeOut(text_banach))
         self.play(text_title.text.animate.to_edge(UP))
-        self.wait(text_title.wait_time)
+        self.wait(1)
 
+
+        # add scene 2
+        # ------------------------------
+        # 2-1 : defination of Banach Spaces (which is a a complete normed space.)
+        text_banachDefination = TextScene(2, "A Complete Normed Space.", 1, "subtitle")
+        text_banachDefination.text.move_to(text_title.text.get_center() + DOWN*2)
+        self.play(Write(text_banachDefination.text))
+        self.wait(text_banachDefination.wait_time)
+
+        # 2-2 : delete "A" from defination and split the rest to 2 parts (Complete + Normed Space)
+        text_normedSpace = Text("Normed Space", **StyleSetter_BasedOnTextType("subtitle"))
+        text_complete = Text("Complete", **StyleSetter_BasedOnTextType("subtitle"))
+        text_normedSpace.move_to(text_banachDefination.text.get_center() + RIGHT*2)
+        text_complete.move_to(text_banachDefination.text.get_center() + LEFT*2)
+        text_normed_complete_group = VGroup(text_normedSpace, text_complete)
+        self.play(Transform(text_banachDefination.text, text_normed_complete_group))
+
+        # 2-3 : make normed space faint and bold Complete
+        self.play(
+            FadeOut(text_banachDefination.text),
+            FadeIn(text_complete),
+            FadeIn(text_normedSpace)
+        )
+
+        self.play(
+            text_normedSpace.animate
+                .set_color(GRAY_C)
+                .set_opacity(0.1)
+                .set_font_size(34),
+
+            text_complete.animate
+                .set_weight(BOLD)
+                .set_font_size(48)
+        )
+
+        self.wait(1)
 
         
