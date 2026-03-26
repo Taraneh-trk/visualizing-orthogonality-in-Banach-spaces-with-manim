98cf87b4f2b6fc1709c56bced8e2c440775d46dd
Taraneh <145665647+Taraneh-trk@users.noreply.github.com>
2026-03-26 14:55:17 +0330

    step 30 : complete banach examples part

--- codes/examples_banach.py
+++ codes/examples_banach.py
@@ -71,10 +71,39 @@ class TitleScene(Scene):
             )
         if return_not_show==True:
             return group, box
+        
+    def show_minorPoint(self,body,return_notShow):
+        """Show message with box to ask a question. """
+
+        title_text = Text("Note", color=dark_blue, font_size=36)
+
+        group = VGroup(title_text, body).arrange(DOWN, buff=0.3)
+
+        box = RoundedRectangle(
+            corner_radius=0.3,
+            color=dark_blue,
+            fill_color=light_blue,
+            fill_opacity=0.2,
+            width=group.width + 0.5,
+            height=group.height + 0.5
+        )
+        
+
+        box.move_to(group.get_center())
+
+        if return_notShow :
+            return group, box
+
+        self.play(Create(box), Write(group))
+        self.wait(2)
+        self.play(FadeOut(group), FadeOut(box))
+        return group, box
 
     def construct(self):
 
-        self.scene1()
+        # self.scene1()
+
+        self.scene2()
 
     def scene1(self):
 
@@ -212,21 +241,45 @@ class TitleScene(Scene):
         )
         self.wait(0.5)
 
-        cauchy_sequence_dis = MathTex(r"\|f_i - f_j\| = \left| \, \frac{1}{i} - \frac{1}{j} \, \right|",color=dark_blue).next_to(encounterexample,DOWN)
+        cauchy_sequence_dis = MathTex(r"\|f_i - f_j\| = \left| \, \frac{1}{i} - \frac{1}{j} \, \right|",r"\to 0",color=dark_blue).next_to(encounterexample,DOWN)
         cauchy_sequence = MathTex(r"\text{Cauchy sequence}",color=dark_blue).scale(1.2).move_to(cauchy_sequence_dis.get_center())
         
         self.play(
-            TransformFromCopy(VGroup(*[norm_example1, encounterexample]), cauchy_sequence_dis),
+            TransformFromCopy(VGroup(*[norm_example1, encounterexample]), cauchy_sequence_dis[0]),
         )
         self.wait(0.5)
+        self.play(
+            Write(cauchy_sequence_dis[1])
+        )
         self.play(
             FadeTransform(cauchy_sequence_dis, cauchy_sequence),
         )
         self.wait(0.5)
         convergant_sequence = MathTex(r"\text{NOT}", r"\text{ a convergent sequence}",color=dark_blue).scale(1.2).move_to(cauchy_sequence_dis.get_center()+DOWN)
         convergant_sequence.set_color_by_tex(r"\text{NOT}",dark_red)
+        converge_to = MathTex(
+            r"f_n(x) \to f(x) = ",
+            r"\begin{cases} -1 & x < 0 \\ 0 & x = 0 \\ 1 & x > 0 \end{cases}",
+            # r"\notin C([-1, 1])",
+            color=BLACK,
+        ).move_to(cauchy_sequence_dis.get_center()+1.5*DOWN)
+        continues3_text = MathTex(r"\text{NOT }",r"\text{Continuous}",color=dark_blue).shift(converge_to.get_center() + 0.7*DOWN + 1.5*LEFT)
+        continues3_text.set_color_by_tex(r"\text{NOT }",dark_red)
+        not_in_text = MathTex(r"f_n(x) \to f(x) \notin C([-1,1])",color=dark_blue).move_to(cauchy_sequence_dis.get_center()+DOWN)
         self.play(
-            TransformFromCopy(cauchy_sequence, convergant_sequence),
+            Write(converge_to),
+        )
+        self.wait(0.5)
+        self.play(
+            Write(continues3_text),
+        )
+        self.wait(0.5)
+        self.play(
+            FadeTransform(VGroup(*[continues3_text, converge_to]), not_in_text),
+        )
+        self.wait(0.5)
+        self.play(
+            TransformMatchingShapes(VGroup(*[not_in_text]), convergant_sequence),
         )
         self.wait(0.5)
 
@@ -240,5 +293,164 @@ class TitleScene(Scene):
         )
         self.wait(0.5)
 
+        fade_out_list = [
+            text,
+            box,
+            space_c_1_1,
+            definition_text[1],
+            norm_example1,
+            banach_space,
+        ]
+        title_example2 = Tex(
+            "Example 2",
+            color=dark_green, font_size=80
+        ).to_edge(UP)
+        self.play(
+            FadeTransform(title_example1, title_example2),
+            FadeOut(VGroup(*fade_out_list)),
+            FadeOut(image_cross),
+        )
 
+        self.wait(1)
 
+    def scene2(self):
+        title_example2 = Tex(
+            "Example 2",
+            color=dark_green, font_size=80
+        ).to_edge(UP)
+        self.add(title_example2)
+        plane = NumberPlane(
+            y_range=[-6, 10, 1],
+            x_range=[-10, 10, 1],
+            background_line_style={
+                "stroke_color": GRAY,
+                "stroke_opacity": 0.5
+            },
+            y_length=10,
+            x_length=15,
+        ).move_to(DOWN)
+        self.add(plane)
+
+        circle_metric = Circle(4,dark_pink,fill_color=dark_pink,fill_opacity=0.1)
+        text_metric_space = MathTex(r"\text{Metric space}",color=dark_pink).scale(2).move_to(circle_metric.get_center()+2.3*UP)
+
+        circle_norm = Circle(2.8,dark_purple,fill_color=dark_purple,fill_opacity=0.1).shift(1*DOWN)
+        text_norm_space = MathTex(r"\text{Normed space}",color=dark_purple).scale(1.5).move_to(circle_norm.get_center()+1.3*UP)
+
+        circle_banach = Circle(1.8,dark_orange,fill_color=dark_orange,fill_opacity=0.1).shift(1.8*DOWN)
+        text_banach_space = MathTex(r"\text{Banach space}",color=dark_orange).scale(1.2).move_to(circle_banach.get_center()+0.2*UP)
+
+        all_shapes = VGroup(*[
+            circle_metric,
+            text_metric_space,
+            circle_norm,
+            text_norm_space,
+            circle_banach,
+            text_banach_space
+        ]).scale(0.7).to_corner(DR)
+
+        self.play(
+            FadeIn(all_shapes),
+        )
+
+        space_definition_part1 = MathTex(
+            r"\text{Space }",
+            r"C ( [-1,1] )",
+            color=dark_pink,
+        ).scale(2)
+        # space_definition_part2 = MathTex(
+        #     r"C ( [-1.1] )",
+        #     color=dark_pink,
+        # ).scale(1.5)
+        space_definition_part3 = MathTex(
+            r" + ",
+            color=dark_pink,
+        ).scale(2)
+        space_definition_part4 = MathTex(
+            r"\text{The limit of every sequence} \\ \text{of continuous functions   }",
+            color=dark_pink,
+        ).scale(1.25)
+
+        space_group = VGroup(space_definition_part1, #space_definition_part2,
+                             space_definition_part3, space_definition_part4).arrange(DOWN,buff=0.4)
+        space_group.next_to(all_shapes,LEFT,buff=0.5).shift(UP+0.1*RIGHT)
+
+        l1space = MathTex(
+            # r"L^1 - space",
+            r"L^1([-1,1])",
+            color=dark_pink,
+        ).scale(1.5).move_to(title_example2.get_center()+3*LEFT+1*DOWN)
+
+        self.play(
+            TransformFromCopy(text_metric_space, space_group),
+        )
+        self.wait(0.5)
+        self.play(
+            TransformMatchingShapes(space_group, l1space)
+        )
+
+        note_l1space_part1 = MathTex(
+            r"\text{In } L^1 - \text{space two functions}",
+            color=BLACK,
+        ).scale(1.2)
+        note_l1space_part2 = MathTex(
+            r"\text{are considered the same if} ",
+            color=BLACK,
+        ).scale(1.2)
+        note_l1space_part3 = MathTex(
+            r"\text{they are equal }",
+            r"\text{almost}",
+            color=BLACK,
+        ).scale(1.2)
+        note_l1space_part3.set_color_by_tex(r"\text{almost}",dark_red)
+        note_l1space_part4 = MathTex(
+            r"\text{everywhere.}",
+            color=BLACK,
+        ).scale(1.2)
+        note_l1space = VGroup(*[note_l1space_part1, note_l1space_part2, note_l1space_part3, note_l1space_part4]).arrange(DOWN,buff=0.2)
+        text, box = self.show_minorPoint(note_l1space, True)
+        text.next_to(l1space,DOWN,buff=0.3)
+        box.move_to(text.get_center())
+        VGroup(*[text,box]).scale(0.9)
+        self.play(
+            Create(box),
+            Write(text),
+        )
+        self.wait(1)
+        self.play(
+            FadeOut(VGroup(*[text,box])),
+        )
+        self.wait(0.5)
+
+        norm_example1 = MathTex(r"\|f\| = \int_{-1}^{1} |f(x)| \, dx",color=dark_purple).scale(1.2).next_to(l1space,DOWN)
+
+        self.play(
+            TransformFromCopy(text_norm_space, norm_example1),
+        )
+        self.wait(1)
+
+        banach_space = MathTex(r"\text{Banach space}",color=dark_orange).scale(1.2).next_to(norm_example1,DOWN,buff=0.5)
+        image_check = ImageMobject("images/green_tik.png").scale(0.1).next_to(banach_space,RIGHT).shift(0.2*UP)
+        self.play(
+            TransformFromCopy(text_banach_space, banach_space),
+        )
+        self.wait(0.5)
+        self.add(image_check)
+        self.wait(1)
+
+        fade_out_list = [
+            all_shapes,
+            l1space,
+            norm_example1,
+            banach_space,
+            title_example2,
+        ]
+        self.play(
+            FadeOut(VGroup(*fade_out_list)),
+            FadeOut(image_check),
+        )
+        self.play(
+            FadeOut(plane),
+        )
+
+        self.wait(1)
